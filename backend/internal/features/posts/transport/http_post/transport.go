45a017a36843585979d080f5f0e4610719c21445
Danil <napalkov-danil@list.ru>
2026-08-16 19:22:56 +0300

    id post

--- backend/internal/features/posts/transport/http_post/transport.go
+++ backend/internal/features/posts/transport/http_post/transport.go
@@ -35,7 +35,7 @@ func (handler *PostHandler) Routers() []server.Route {
 		},
 		{
 			Method:  "GET",
-			Path:    "/posts/{}",
+			Path:    "/posts/{id}",
 			Handler: handler.GetPostsByType,
 		},
 		{
